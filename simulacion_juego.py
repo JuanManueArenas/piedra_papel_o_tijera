b544b1afce4ff528e71d102ccc2a6877df4168ef
JuanManueArenas <juanarenas@colegioguanenta.edu.co>
2023-03-21 22:15:52 -0500

    se agrego el diagrama al README y el programa

--- simulacion_juego.py
+++ simulacion_juego.py
@@ -8,21 +8,40 @@ print("------------------------------------------")
 #input
 
 bot = random.randint(1,3)
-user = int(input("agrega tu opcion"))
+user = int(input("Agregaste el numero "))
  
 # Processing
+if (user>3):
+    rta = "usted digito un numero no valido"
+
 if (bot==1): #piedra
     if(user==1):
         rta = "EMPATE"
     elif(user==2):
         rta = "GANASTE"
-    else:rta = "perdiste"
+    else: 
+        rta = "perdiste"
 elif (bot==2): #papel
     if(user==1):
        rta = "GANASTE"
     elif(user==2):
         rta = "EMPATE"
-        
+    else:
+        rta = "PERDISTE"
+elif (bot==3): #tijera
+    if(user==1):
+        rta = "PERDISTE"
+    elif(user==2):
+        rta = "GANASTE"
+    else:
+        rta = "EMPATE"   
+
+
+#output
+print(rta)
+print("Bot : " +str(bot))
+print("USTED DIGITO: " +str(user))
+    
 
 
 
